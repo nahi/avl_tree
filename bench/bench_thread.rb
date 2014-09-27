117c4f09a4e37187ec746a7b3862c37be06b090e
Hiroshi Nakamura <nahi@ruby-lang.org>
2014-09-27 21:50:20 +0900

    Add multi-thread benchmark
     * 2 Threads for insert
     * 2 Threads for delete
     * 2 Threads for read

---
+++ bench/bench_thread.rb
@@ -0,0 +1,40 @@
+require 'benchmark'
+require 'red_black_tree'
+
+GC.disable
+Benchmark.bmbm do |bm|
+  bm.report do
+    h = RedBlackTree.new
+    num = 100000
+    max = 1000
+    threads = []
+    # writers
+    2.times do
+      threads << Thread.new {
+        num.times do
+        key = rand(max)
+        h[key] = key
+        end
+      }
+    end
+    # deleters
+    2.times do
+      threads << Thread.new {
+        num.times do
+        key = rand(max)
+        h.delete(key)
+        end
+      }
+    end
+    # readers
+    2.times do
+      threads << Thread.new {
+        num.times do
+        key = rand(max)
+        h[key]
+        end
+      }
+    end
+    threads.each(&:join)
+  end
+end
